e0145379ab6a8a45a621dcb4ed836cf53867f9da
Mahesh Kumar <Mahesh.kumar@Mastek.Com>
2023-02-11 21:26:35 +0530

    Added separate library for exceptions.

---
+++ BatchDemo.Logger/GlobalExceptionMiddleware.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Serilog.Core;
+
+namespace BatchDemo.Logger
+{
+    public static class GlobalExceptionMiddleware
+    {
+        public static void UseGlobalExceptionMiddleware(this IApplicationBuilder app,Serilog.Core.Logger logger)
+        {
+            app.UseMiddleware<ExceptionMiddleware>(logger);
+        }
+    }
+}
